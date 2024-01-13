338c5a63f6c2c38b688a4096eb2bc65bca617b21
Nyrolla <alloryn.mappin@me.com>
2024-01-13 10:45:11 -0700

    Create Categories in database and connected to project

--- DataAccess/AppicationDbContext.cs
+++ DataAccess/AppicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Infrastructure.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,7 +12,17 @@ namespace DataAccess
     {
         public AppicationDbContext(DbContextOptions<AppicationDbContext> options) :base(options)
         {
+
             
         }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("your_connection_string_here",
+                    b => b.MigrationsAssembly("CBTDWeb")); // Specify your target project name
+            }
+        }
+        public DbSet<Category> Categories { get; set; } //The physical tabel
     }
 }
